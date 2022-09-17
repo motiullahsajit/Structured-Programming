2ec6bc79f17ebe72fc17384869ba7619df6c1609
motiullahsajit <sajitmotiullah@gmail.com>
2022-09-17 14:26:15 +0600

    solution updated

--- Complex Condition Practice/practice_4.c
+++ Complex Condition Practice/practice_4.c
@@ -2,48 +2,28 @@
  A C program to print day name of week
  Author : Motiullah Sajit
  */
-
 #include <stdio.h>
 
 int main()
 {
-    int weekDayNum;
+    int weekNum;
 
-    printf("Enter week number (1-7): ");
-    scanf("%d", &weekDayNum);
+    printf("Enter week number (1-52): ");
+    scanf("%d", &weekNum);
 
-
-    if(weekDayNum == 1)
+    if(weekNum > 0 && weekNum <=52)
     {
-         printf("Saturday");
-    }
-    else if(weekDayNum == 2)
-    {
-         printf("Sunday");
-    }
-    else if(weekDayNum == 3)
-    {
-        printf("Monday");
-    }
-    else if(weekDayNum == 4)
-    {
-        printf("Tuesday");
-    }
-    else if(weekDayNum == 5)
-    {
-        printf("Wednesday");
-    }
-    else if(weekDayNum == 6)
-    {
-        printf("Thursday");
-    }
-    else if(weekDayNum == 7)
-    {
-        printf("Friday");
+        printf("Saturday\n");
+        printf("Sunday\n");
+        printf("Monday\n");
+        printf("Tuesday\n");
+        printf("Wednesday\n");
+        printf("Thursday\n");
+        printf("Friday\n");
     }
     else
     {
-        printf("Invalid Input! Please enter week number between 1-7.");
+        printf("Invalid Input! Please enter week number between 1-52.");
     }
 
     return 0;
